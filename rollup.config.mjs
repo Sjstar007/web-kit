dfc4b24aca6702a3b306c0503f91cf39dad91217
ShubhamJangid <shubham.ext1@bijnis.com>
2023-06-29 16:16:57 +0530

    first commit to webkit lib

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const extensions = ['.js', '.jsx'];
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/my-component-library.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/my-component-library.esm.js',
+      format: 'esm',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    babel({ extensions, babelHelpers: 'bundled', presets: ['@babel/preset-react'] }),
+    resolve({ extensions }),
+    commonjs(),
+  ],
+  external: ['react', 'react-dom'],
+};
